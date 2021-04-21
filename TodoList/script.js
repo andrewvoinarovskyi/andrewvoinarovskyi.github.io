1d65bb107c6ae439d3bdd5d910399568f8a52698
AndrewVoinarovskyi <avoinarovskiy@gmail.com>
2021-04-21 14:16:24 +0300

    Added event control DOM

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,3 +1,5 @@
+const todoListElement = document.querySelector('#list');
+const TRASH = '\u{1F5D1}';
 class todoItem {
     constructor (id, title, description, dueDate, done) {
         this.id = id;
@@ -8,26 +10,53 @@ class todoItem {
     }
 }
 
-function appendTodoItem(item) {
-    const {id, title, description, dueDate, done} = item;
-
-    let taskDescription = description ? `<p class="description">${description}</p>` : `<p class="description"></p>`;
-    let taskDueDate = dueDate ? `<p class="date${dueDate <= Date.now() ? " overdue-date": ''}">${dueDate.toDateString()}</p>` :`<p class = "date"></p>`;
-    let checkbox = `<input type="checkbox"${done ? ' checked' : 'unchecked'} />`;
-    let taskTitle = `<p class="title${done ? ' line-through' : ''}">${title.toUpperCase()}</p>`;
-    const inner = `<section id="item_${id}">` + checkbox + taskTitle + taskDescription + taskDueDate + `</p></section>`;
-
-    console.log(inner);
-    todoListElement.innerHTML += inner;
-}
-
 let todoList = [
     new todoItem(1, `describe array of todos`, 'some description', '', true),
     new todoItem(2, `add todos in array`, 'some another description', '2021-05-01', false),
     new todoItem(3, `make output's layout`, '', '2021-04-19', false),
 ];
 
-const todoListElement = document.querySelector('#list');
+render(todoList);
 
+function render (list) {
+    todoListElement.innerHTML = "";
 
-todoList.forEach(appendTodoItem);
+    list.forEach(appendTodoItem);
+
+    let checkbox = document.querySelectorAll('.checkbox');
+
+    checkbox.forEach(box => box.addEventListener('click', (event) => {
+        box.parentElement.classList.toggle('done');
+    }));
+
+    let deleteButton = document.querySelectorAll('.delete');
+
+    deleteButton.forEach(btn => btn.addEventListener('click', (event) => {
+        deleteItem(btn.parentElement);
+        event.stopPropagation();
+    }));
+}
+
+function hideMade() {
+    document.querySelector('#list').classList.toggle('only-open');
+}
+
+function deleteItem(element) {
+    todoList = todoList.filter(todoItem => todoItem['id'].toString() !== element.id.split('_')[1]);
+    element.parentElement.removeChild(element);
+}
+
+function appendTodoItem(item) {
+    if (item !== null) {
+        const {id, title, description, dueDate, done} = item;
+
+        let checkbox = `<input class="checkbox" type="checkbox" ${done ? 'checked' : 'unchecked'} />`;
+        let taskTitle = `<p class="title">${title.toUpperCase()}</p>`;
+        let taskDescription = description ? `<p class="description">${description}</p>` : `<p class="description"></p>`;
+        let taskDueDate = dueDate ? `<p class="date${dueDate <= Date.now() ? " overdue-date" : ''}">${dueDate.toDateString()}</p>` : `<p class = "date"></p>`;
+
+        const inner = `<section class="item${done ? ' done' : ''}" id="item_${id}">` + checkbox + taskTitle + taskDescription + taskDueDate + `<button class="delete">${TRASH}</button></section>`;
+
+        todoListElement.innerHTML += inner;
+    }
+}
